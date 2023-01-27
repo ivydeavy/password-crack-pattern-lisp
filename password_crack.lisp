031ed145f5785359e8313e8cf2db19b881b67d3d
Deepraj Das <mail30deeprajdas@gmail.com>
2023-01-27 21:21:53 +0530

    Update password_crack.lisp

--- password_crack.lisp
+++ password_crack.lisp
@@ -1,3 +1,4 @@
+;Password crack display pattern in LISP.
 (defvar *characters* '(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z #\space #\\ #\` #\~ #\! #\@ #\# #\$ #\% #\^ #\& #\* #\( #\) #\- #\_ #\+ #\* #\/ #\{ #\} #\[ #\] #\: #\; #\" #\' #\< #\, #\. #\> #\? #\/ ))
 
 (defparameter string-found "")
