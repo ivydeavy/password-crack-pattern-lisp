5e8e71c1923c72ee8c9f207578bde637490511f7
Deepraj Das <mail30deeprajdas@gmail.com>
2023-01-31 20:08:18 +0530

    bug fixed.

--- password_crack.lisp
+++ password_crack.lisp
@@ -1,22 +1,20 @@
-;Password crack display pattern in LISP.
-(defvar *characters* '(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z #\space #\\ #\` #\~ #\! #\@ #\# #\$ #\% #\^ #\& #\* #\( #\) #\- #\_ #\+ #\* #\/ #\{ #\} #\[ #\] #\: #\; #\" #\' #\< #\, #\. #\> #\? #\/ ))
+(defvar *characters* '(#\Space #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z #\\ #\` #\~ #\! #\@ #\# #\$ #\% #\^ #\& #\* #\( #\) #\- #\_ #\+ #\* #\/ #\{ #\} #\[ #\] #\: #\; #\" #\' #\< #\, #\. #\> #\? #\/ #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9))
 
-(defparameter string-found "")
+(defparameter *string-found* "")
 
-(defun loop-through(string)
+(defun loop-through (string)
 (setq string-length (length string))
 (dotimes (i string-length)
 (dolist (j *characters*) 
 (progn
 (sleep (* 0.025))
-(print (format nil "~{~a~}" (list string-found j)))
+(format t "~{~a~}~%" (list *string-found* j))
 (if (equal j (aref string i))
 (progn 
-(setq string-found (format nil "~{~a~}" 
-(list string-found j)))
-(print string-found)
+(setq *string-found* 
+(format nil "~{~a~}" (list *string-found* j)))
 (return))))))
-(setq string-found "")
+(setq *string-found* "")
 (main))
 
 
@@ -24,6 +22,6 @@
 (print "Annyeong! Welcome to LISP Crack Password Pattern!")
 (print "Made by Deepraj")
 (print "Enter string: ")
-(loop-through (string-upcase (read))))
+(loop-through  (string-upcase (read-line))))
 
 (main)
